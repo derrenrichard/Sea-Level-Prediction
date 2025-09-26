bda1c85701bb522fa376c70acc9a2cea061022fe
derrenrichard <60237479+derrenrichard@users.noreply.github.com>
2025-09-26 18:55:24 +0700

    Delete FreeCodeCamp directory

--- FreeCodeCamp/Project/main.py
+++
@@ -1,9 +0,0 @@
-# This entrypoint file to be used in development. Start by reading README.md
-import sea_level_predictor
-from unittest import main
-
-# Call the draw_plot function to generate the plot
-sea_level_predictor.draw_plot()
-
-# Run unit tests automatically
-main(module='test_module', exit=False)
